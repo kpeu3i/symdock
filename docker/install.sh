02aab3fd3c78ebcbf72a39b971229cbd82fa98f6
Andrey Bukatov <andrey.bukatov@gmail.com>
2016-01-11 12:25:02 +0200

    Fix ignore folders list Fix symfony installation script

--- docker/install.sh
+++ docker/install.sh
@@ -3,5 +3,7 @@
 # https://github.com/docker/compose/issues/45
 PROJECT=$(head -n 1 ./project)
 
+rm -f ../symfony/.gitkeep
+
 docker exec -u www-data -it "$PROJECT"_symfony_1 composer create-project symfony/framework-standard-edition . "2.8.*"
 
